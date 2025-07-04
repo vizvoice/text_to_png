fa38d8efea1e0c718918e65223dc38d4041ac5a1
vizvoice <jasper@vizvoice.com>
2025-07-04 16:10:12 +0200

    Update index.js
    Added poppins

--- src/index.js
+++ src/index.js
@@ -91,7 +91,7 @@ app.post('/generate', (req, res) => {
     color: color,
     backgroundColor: 'transparent',
     textAlign: 'center',
-    fontPath: './fonts/Poppins-BoldItalic.ttf'
+    fontPath: './src/fonts/Poppins-BoldItalic.ttf'
   });
 
   res.set('Content-Type', 'image/png');
