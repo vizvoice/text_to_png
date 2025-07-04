c659f8875d0d7c883bd81ab69ed57ac594feecd8
vizvoice <jasper@vizvoice.com>
2025-07-04 16:27:28 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const text2png = require('text2png');
+const path = require('path');
 const app = express();
+
 app.use(express.json());
 
 const presets = {
@@ -33,40 +35,40 @@ const presets = {
     height: 3600,
   },
   hoodie: {
-  font: 'bold 70px Poppins',
-  padding: 300,
-  lineSpacing: 20,
-  width: 3600,
-  height: 4800,
-},
-poster: {
-  font: 'bold 100px Poppins',
-  padding: 400,
-  lineSpacing: 30,
-  width: 4800,
-  height: 6400,
-},
-sticker: {
-  font: 'bold 60px Poppins',
-  padding: 100,
-  lineSpacing: 15,
-  width: 2000,
-  height: 2000,
-},
-notebook: {
-  font: 'bold 70px Poppins',
-  padding: 150,
-  lineSpacing: 15,
-  width: 1600,
-  height: 2400,
-},
-babybody: {
-  font: 'bold 50px Poppins',
-  padding: 200,
-  lineSpacing: 15,
-  width: 2800,
-  height: 3600,
-},
+    font: 'bold 70px Poppins',
+    padding: 300,
+    lineSpacing: 20,
+    width: 3600,
+    height: 4800,
+  },
+  poster: {
+    font: 'bold 100px Poppins',
+    padding: 400,
+    lineSpacing: 30,
+    width: 4800,
+    height: 6400,
+  },
+  sticker: {
+    font: 'bold 60px Poppins',
+    padding: 100,
+    lineSpacing: 15,
+    width: 2000,
+    height: 2000,
+  },
+  notebook: {
+    font: 'bold 70px Poppins',
+    padding: 150,
+    lineSpacing: 15,
+    width: 1600,
+    height: 2400,
+  },
+  babybody: {
+    font: 'bold 50px Poppins',
+    padding: 200,
+    lineSpacing: 15,
+    width: 2800,
+    height: 3600,
+  },
 };
 
 app.post('/generate', (req, res) => {
@@ -77,13 +79,12 @@ app.post('/generate', (req, res) => {
 
   const config = presets[product] || presets.tshirt;
 
-  // Automatisch regelsplitsing
+  // Regelsplitsing
   const words = inputText.split(' ');
   const lines = [];
   for (let i = 0; i < words.length; i += maxWordsPerLine) {
     lines.push(words.slice(i, i + maxWordsPerLine).join(' '));
   }
-
   const multilineText = lines.join('\n');
 
   const buffer = text2png(multilineText, {
@@ -91,11 +92,13 @@ app.post('/generate', (req, res) => {
     color: color,
     backgroundColor: 'transparent',
     textAlign: 'center',
-    fontPath: __dirname + '/fonts/Poppins-BoldItalic.ttf'
+    fontPath: path.join(__dirname, 'fonts', 'Poppins-BoldItalic.ttf'),
   });
 
   res.set('Content-Type', 'image/png');
-res.set('Content-Disposition', 'attachment; filename="output.png"');
-res.send(buffer);
+  res.set('Content-Disposition', 'attachment; filename="output.png"');
+  res.send(buffer);
 });
 
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
